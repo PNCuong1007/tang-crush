5cde236e01f8fc58000df80424717990becb7e64
PNCuong1007 <86774886+PNCuong1007@users.noreply.github.com>
2021-07-02 15:11:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Thứ 7 tuần sau là một ngày đẹp trời',
+    desc: 'Chị đi dạo vài vòng với anh đc không??? ',
+    btnYes: 'Chị đi được nè',
+    btnNo: 'Dịch dã như này đi đâu mà đi',
+    question:'Dịch dã như này mà chị vẫn muốn đi à???',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'Đi thì đi không đi thì đi🥰🥰🥰',
+    mess: 'Hì hì!!! Yêu chị nhiều nhiều 😘😘',
+    messDesc: '7h anh qua đón chị nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/1868066449961547/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
